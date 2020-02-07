431131d3eb3161cb8faa6297f5e6e2f7d9eeaa31
DevynWright <Wrightdevyn@gmail.com>
2020-02-07 14:23:04 -0700

    setting up

--- wwwsrc/src/authConfig.js
+++ wwwsrc/src/authConfig.js
@@ -1,3 +1,3 @@
-export const domain = "";
-export const audience = "";
-export const clientId = "";
+export const domain = "devyn.auth0.com";
+export const audience = "https://devyn.auth0.com/api/v2/";
+export const clientId = "cw3FOZaPSxZ5dJO9mTiQHYnFXo3uE4cp";
