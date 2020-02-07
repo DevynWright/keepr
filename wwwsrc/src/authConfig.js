38340140709c0126bd033424ff1818ab884e619e
DevynWright <Wrightdevyn@gmail.com>
2020-02-07 15:01:31 -0700

    set up db

--- wwwsrc/src/authConfig.js
+++ wwwsrc/src/authConfig.js
@@ -1,3 +1,3 @@
 export const domain = "devyn.auth0.com";
-export const audience = "https://devyn.auth0.com/api/v2/";
+export const audience = "https://keepr-api.com";
 export const clientId = "cw3FOZaPSxZ5dJO9mTiQHYnFXo3uE4cp";
